6fd196505adc3c824093de2194a2d34920e9fead
Valentin Mandev <52213186+ValentinMandev@users.noreply.github.com>
2020-03-06 14:30:37 +0200

    Add files via upload

--- get_titles_and_links.py
+++ get_titles_and_links.py
@@ -23,7 +23,7 @@ with open('all_songs.txt', 'w') as outfile:
 # Обработка на информацията от файла - премахваме излишни неща, пълним отделни променливи за име на изпълнител, име на песен, линк и
 # ID (което взимаме от ID-то от линка с текста на песента).
 # Пълним списък от списъци с необходимата ни информация
-artist_song_list = [[] for _ in range(10000)] # тук дефинирам някаква голяма бройка списъци съдържащи се в основния списък, за да съм сигурен, че няма да ми даде out of range. Списъка на песни е повече от два пъти по-малко.
+artist_song_list_raw = [[] for _ in range(10000)] # тук дефинирам някаква голяма бройка списъци съдържащи се в основния списък, за да съм сигурен, че няма да ми даде out of range. Списъка на песни е повече от два пъти по-малко.
 line_number = 0 # това го ползвам, за да управлявам кой по ред списък част от основния списък да пълни
 with open('all_songs.txt') as input_file:
     for line in input_file:
@@ -34,18 +34,25 @@ with open('all_songs.txt') as input_file:
                 artist_name = txt[:txt.index(' - <a href="?lyrics=')]
                 song_name = txt[len(txt) - txt[::-1].index('>'):].replace('\n', '')
                 lyrics_link = 'https://www.eurobeat-prime.com/lyrics.php?lyrics=' + song_id
-                artist_song_list[line_number].append(song_id)
-                artist_song_list[line_number].append(artist_name)
-                artist_song_list[line_number].append(song_name)
-                artist_song_list[line_number].append(lyrics_link)
+                artist_song_list_raw[line_number].append(song_id)
+                artist_song_list_raw[line_number].append(artist_name)
+                artist_song_list_raw[line_number].append(song_name)
+                artist_song_list_raw[line_number].append(lyrics_link)
                 line_number += 1
 
+# Изчиствам си празните списъци от списъка с данни
+artist_song_list = list()
+for lst in range(len(artist_song_list_raw)):
+    if len(artist_song_list_raw[lst]) > 0:
+        artist_song_list.append(artist_song_list_raw[lst])
 
+# Записвам си един файл с всички песни.
 with open('songs_available.txt', 'w') as output_file:
     for songs in range(len(artist_song_list)):
         if artist_song_list[songs]:
             print(artist_song_list[songs][0] + ': "' + str(artist_song_list[songs][1]).replace(':', '-') + ' - ' + str(artist_song_list[songs][2]).replace(':', '-') + '"', file=output_file)
 
+# Изтривам ненужния all_songs.txt
 os.remove("all_songs.txt")
 
 
--- update_songs_database.py
+++ update_songs_database.py
@@ -2,21 +2,17 @@ import sqlite3
 from get_titles_and_links import artist_song_list
 import os
 
+# Изчиствам си базата данни, когато я свалям наново
 os.remove("all_songs.db")
 
-data = list()
-for lst in range(len(artist_song_list)):
-	if len(artist_song_list[lst]) > 0:
-		data.append(tuple(artist_song_list[lst]))
 
 connector = sqlite3.connect("all_songs.db")
 cursor = connector.cursor()
 
 
-cursor.execute('''CREATE TABLE all_songs
-             (song_id NUMERIC NOT NULL, artist_name text, song_name text, lyrics_link text)''')
+cursor.execute('''CREATE TABLE all_songs (song_id NUMERIC NOT NULL, artist_name text, song_name text, lyrics_link text)''')
 
-cursor.executemany("INSERT INTO all_songs VALUES (?,?,?,?)", data)
+cursor.executemany("INSERT INTO all_songs VALUES (?,?,?,?)", artist_song_list)
 
 connector.commit()
 
