60672ecfa56fe87770d022150ca5e745af764dcd
Valentin Mandev <52213186+ValentinMandev@users.noreply.github.com>
2020-03-08 01:45:03 +0200

    Add files via upload

--- get_info_from_excel_database.py
+++ get_info_from_excel_database.py
@@ -1,6 +1,14 @@
 import urllib.request
+import openpyxl
 
-url = 'https://drive.google.com/u/0/uc?id=1HAuDZ-jPWco90VF1A9yfBwlXgxdP5Obu&export=download'
+url = 'https://drive.google.com/u/0/uc?id=1ImiITsoRt5CwaEUEgYJ__QUFZbaZ2pct&export=download'
 excel_database = 'full_database.xlsx'
 
 urllib.request.urlretrieve(url, excel_database)
+
+workbook = openpyxl.load_workbook(filename=excel_database)
+sheets = workbook.sheetnames
+
+for sheet in sheets:
+    print(workbook[sheet]['A2'].value)
+
