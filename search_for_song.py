4e12c49163dd3fc450d67a359885083d2ad2b571
Valentin Mandev <52213186+ValentinMandev@users.noreply.github.com>
2020-03-09 02:16:15 +0200

    Add files via upload

--- search_for_song.py
+++ search_for_song.py
@@ -13,5 +13,5 @@ search_field = input('Search for a song, artist, year or label ')
 for songs in range(len(results['song_name'])):
     s = re.compile(search_field, re.I)
     if s.search(results['song_name'][songs]) or s.search(results['artist_name'][songs]) or s.search(str(results['release_year'][songs])) or s.search(results['label'][songs]):
-        print(results['artist_name'][songs], '-', results['song_name'][songs], f"{'(' + str(results['release_year'][songs]) + ')' if results['release_year'][songs] is not None else ''}")
+        print(results['artist_name'][songs], '-', results['song_name'][songs], f"{'(' + str(results['release_year'][songs]) + ')' if results['release_year'][songs] is not None and results['release_year'][songs] is not '' else ''}")
 
