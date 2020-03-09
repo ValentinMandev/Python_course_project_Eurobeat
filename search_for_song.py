8c11b233fb7264ba6508aedb12cd539ed6bc36be
Valentin Mandev <52213186+ValentinMandev@users.noreply.github.com>
2020-03-09 18:52:23 +0200

    Add files via upload

--- search_for_song.py
+++ search_for_song.py
@@ -18,18 +18,33 @@ results = pd.DataFrame.from_records(data=query.fetchall(), columns=cols)
 #     if s.search(results['song_name'][songs]) or s.search(results['artist_name'][songs]) or s.search(str(results['release_year'][songs])) or s.search(results['label'][songs]):
 #         print(results['artist_name'][songs], '-', results['song_name'][songs], f"{'(' + str(results['release_year'][songs]) + ')' if results['release_year'][songs] is not None and results['release_year'][songs] is not '' else ''}")
 
+# Всеки мач да има ID, да се пълни речник с ID-та и песни, избираме ID и по него дава информация
 
-song_id = 4069
-# Минаваме през всички страници и записваме файл с имена и линкове за всички песни:
+song_id = 69
 with open('temporary_lyrics_file.txt', 'w') as tempfile:
     lyrics_page = urllib.request.urlopen(f'https://www.eurobeat-prime.com/lyrics.php?lyrics={song_id}')
     soup = BeautifulSoup(lyrics_page, 'html.parser')
     text = soup.findAll("div", class_="mmids")
 txt = str(text).replace('</a><br/>', '').replace('&amp;', '&').replace('<b>#:</b><p align="left">', '').replace('[<div class="mmids">\n', '').replace('</div>]', '').replace('<br/>', '')
 
-
 with open('updated_lyrics_file.txt', 'w') as outfile:
     end_title_index = txt.index('>Search database') + len('>Search database')
     print(txt[end_title_index:], file=outfile)
+
+
+for songs in range(len(results['song_id'])):
+    if results['song_id'][songs] == song_id:
+        print(f"{results['artist_name'][songs]} - {results['song_name'][songs]}")
+        if results['release_year'][songs] is not None: print(f"First release: {results['release_year'][songs]}")
+        if results['label'][songs] is not None: print(f"Music company: {results['label'][songs]}")
+        if results['producer'][songs] is not None: print(f"Produced by: {results['producer'][songs]}")
+        if results['song_writer'][songs] is not None: print(f"Song writer: {results['song_writer'][songs]}")
+        print(f"YouTube search link: {results['youtube_search_link'][songs]}")
+        if results['duration'][songs] is not None: print(f"Song duration: {results['duration'][songs]}")
+        if song_id < 100000: print(f"Lyrics:\n{txt[end_title_index:]}")
+
+
+
+
 
 
