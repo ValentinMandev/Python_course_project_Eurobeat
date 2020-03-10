051d27663b7e8cb97090732fdb264193f33329a2
Valentin Mandev <52213186+ValentinMandev@users.noreply.github.com>
2020-03-11 01:35:28 +0200

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,5 @@
+import tkinter
+
+# https://realpython.com/python-gui-tkinter/
+# https://www.tutorialspoint.com/python/python_gui_programming.htm
+# https://www.geeksforgeeks.org/python-gui-tkinter/
